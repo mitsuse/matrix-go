17a20def2e212d97205af1933be26afead813a66
Tomoya Kose <tomoya@mitsuse.jp>
2015-06-02 00:02:05 +0900

    Write test functions for "(*matrixImpl).Rows()" and "(*matrixImpl).Columns()".

---
+++ mutable/dense/dense_test.go
@@ -0,0 +1,59 @@
+package dense
+
+import (
+	"testing"
+)
+
+type constructTest struct {
+	rows     int
+	columns  int
+	elements []float64
+}
+
+func createShapeTestSeq() []*constructTest {
+	testSeq := []*constructTest{
+		&constructTest{rows: 2, columns: 2, elements: []float64{0, 1, 2, 3}},
+		&constructTest{rows: 1, columns: 2, elements: []float64{0, 1}},
+		&constructTest{rows: 2, columns: 1, elements: []float64{0, 1}},
+		&constructTest{rows: 3, columns: 1, elements: []float64{0, 1, 2}},
+		&constructTest{rows: 1, columns: 3, elements: []float64{0, 1, 2}},
+		&constructTest{rows: 3, columns: 2, elements: []float64{0, 1, 2, 3, 4, 5}},
+		&constructTest{rows: 2, columns: 3, elements: []float64{0, 1, 2, 3, 4, 5}},
+	}
+
+	return testSeq
+}
+
+func TestRowsSucceedsAlways(t *testing.T) {
+	testSeq := createShapeTestSeq()
+
+	for _, test := range testSeq {
+		m, err := New(test.rows, test.columns)(test.elements...)
+		if err != nil {
+			template := "The number of %q equals to %q * %q, but an error caused."
+			t.Fatalf(template, "elements", "rows", "columns")
+		}
+
+		if rows := m.Rows(); rows != test.rows {
+			template := "The \"rows\" should be %d, but is %d."
+			t.Fatalf(template, test.rows, rows)
+		}
+	}
+}
+
+func TestColumnsSucceedsAlways(t *testing.T) {
+	testSeq := createShapeTestSeq()
+
+	for _, test := range testSeq {
+		m, err := New(test.rows, test.columns)(test.elements...)
+		if err != nil {
+			template := "The number of %q equals to %q * %q, but an error caused."
+			t.Fatalf(template, "elements", "rows", "columns")
+		}
+
+		if columns := m.Columns(); columns != test.columns {
+			template := "The \"columns\" should be %d, but is %d."
+			t.Fatalf(template, test.columns, columns)
+		}
+	}
+}
