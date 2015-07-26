1408a1bcd00156072ede85d547be0149ee60d141
Tomoya Kose <tomoya@mitsuse.jp>
2015-07-26 18:55:48 +0900

    Add the documentation for pacnics.

--- internal/types/matrix.go
+++ internal/types/matrix.go
@@ -20,21 +20,34 @@ type Matrix interface {
 	Diagonal() Cursor
 
 	// Get an element of matrix speficied with "row" and "column".
+	// When "row" or "column" is lower than the number of rows or columns,
+	// validates.OUT_OF_RANGE_PANIC will be caused.
 	Get(row, column int) (element float64)
 
 	// Update the element of matrix speficied with "row" and "column".
+	// When "row" or "column" is lower than the number of rows or columns,
+	// validates.OUT_OF_RANGE_PANIC will be caused.
 	Update(row, column int, element float64) Matrix
 
 	// Check element-wise equality of the receiver matrix and the given matrix.
+	// When the shape of the receiver and the argument is different,
+	// validates.DIFFERENT_SIZE_PANIC will be caused.
 	Equal(n Matrix) bool
 
 	// Add the given matrix to the receiver matrix.
+	// When the shape of the receiver and the argument is different,
+	// validates.DIFFERENT_SIZE_PANIC will be caused.
 	Add(n Matrix) Matrix
 
 	// Subtract the given matrix from the receiver matrix.
+	// When the shape of the receiver and the argument is different,
+	// validates.DIFFERENT_SIZE_PANIC will be caused.
 	Subtract(n Matrix) Matrix
 
 	// Multiply the receiver matrix by the given matrix.
+	// When the number of columns of the receiver doesn't equal to
+	// the number of rows of the argument,
+	// validates.NOT_MULTIPLIABLE_PANIC will be caused.
 	Multiply(n Matrix) Matrix
 
 	// Multiply by scalar value.
