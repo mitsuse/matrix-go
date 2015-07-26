826e418b283a50fab30473e68af72575476ffe19
Tomoya Kose <tomoya@mitsuse.jp>
2015-07-26 11:18:26 +0900

    Add the short description for "Matrix" and "Cursor".

--- matrix.go
+++ matrix.go
@@ -7,10 +7,20 @@ import (
 	"github.com/mitsuse/matrix-go/internal/types"
 )
 
+/*
+"Matrix" is the interface for implementations of various matrix types.
+For more details, refer "types.Matrix".
+*/
 type Matrix interface {
 	types.Matrix
 }
 
+/*
+"Cursor" is the interface for iterator for elements of matrix.
+Some implementations of "Cursor" iterate all elements,
+and others iterates elements satisfying conditions.
+For more details, refer "types.Cursor".
+*/
 type Cursor interface {
 	types.Cursor
 }
